d475da45031dcd5b2a4d52eb413f5c18596bbb41
bai0527 <haku.yoh@sparktimes.co.jp>
2024-11-29 00:24:20 +0900

    決済対応

--- lineapp/config/setting/base.py
+++ lineapp/config/setting/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'api.order', 
     'api.user', 
     'api.chat', 
+    'api.payment', 
     'rest_framework',
 ]
 
@@ -187,6 +188,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
