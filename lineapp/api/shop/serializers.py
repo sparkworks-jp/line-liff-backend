f4602782f606026d4d939ccb3d05349025c4c89b
ro.gyou@harbest+1-at-010662016841 <ro.gyou@harbest.com>
2024-12-10 10:54:23 +0900

    shop関連改善

--- lineapp/api/shop/serializers.py
+++ lineapp/api/shop/serializers.py
@@ -1,7 +1,9 @@
 # serializers.py
-
 from rest_framework import serializers
 from .models import Product
+import logging
+
+logger = logging.getLogger(__name__)
 
 class ProductSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
@@ -11,9 +13,10 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = '__all__'
     
     def get_image(self, obj):
-        if obj.image:
-            if str(obj.image).startswith('https://'):
-                return str(obj.image)
-            return f"{str(obj.image).lstrip('/')}"
-
-        return None
+        if not obj.image:
+            return None            
+        image_url = str(obj.image)
+        if image_url.startswith(('https://', 'http://')):
+            return image_url       
+        logger.warning(f"予期しないイメージURLの形式です: {image_url}")
+        return image_url
--- lineapp/api/shop/views.py
+++ lineapp/api/shop/views.py
@@ -1,24 +1,24 @@
 # views.py
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.exceptions import APIException
+
+from common.exceptions import CustomAPIException
 from .models import Product
 from .serializers import ProductSerializer
 from django.shortcuts import get_object_or_404
 
-# @api_view(['POST'])
-# def create_product(request):
-#     serializer = ProductSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def get_product(request, product_id):
     product = get_object_or_404(Product, product_id=product_id)
+    if not product.is_active:
+        raise CustomAPIException(
+            status=status.HTTP_404_NOT_FOUND,
+            message="商品已下架",
+            severity="warning"
+        )
     serializer = ProductSerializer(product)
     return Response(serializer.data)
 
@@ -26,23 +26,14 @@ def get_product(request, product_id):
   
 def list_products(request):
     products = Product.objects.all()
-    print("原始image值:", products.first().image)
+    if not products.exists():
+        return Response({
+            'message': '商品データはまだありません',
+            'data': []
+        }, status=status.HTTP_200_OK)
+    print("image值:", products.first().image)
     serializer = ProductSerializer(products, many=True)
-    print("序列化后image值:", serializer.data[0]['image'])
+    print("シリアル化image值:", serializer.data[0]['image'])
 
     return Response(serializer.data)
 
-# @api_view(['PUT'])
-# def update_product(request, product_id):
-#     product = get_object_or_404(Product, product_id=product_id)
-#     serializer = ProductSerializer(product, data=request.data, partial=True)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(serializer.data)
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# @api_view(['DELETE'])
-# def delete_product(request, product_id):
-#     product = get_object_or_404(Product, product_id=product_id)
-#     product.delete()
-#     return Response({"message": "Product deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
